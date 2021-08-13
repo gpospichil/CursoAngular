ef4a840bd41d07fc496f94a861d1cb8b1bdcc9c9
gpospichil <87378774+gpospichil@users.noreply.github.com>
2021-08-13 17:44:57 -0300

    Projeto final

--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, OnDestroy } from '@angular/core';
 import { Frase } from '../shared/frase.model';
 import { FRASES } from './frases-mock';
 
@@ -7,36 +7,58 @@ import { FRASES } from './frases-mock';
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
-export class PainelComponent implements OnInit {
+export class PainelComponent implements OnInit, OnDestroy {
+
+  @Output()
+  public encerrarJogo: EventEmitter<string> = new EventEmitter()
 
   public frases: Frase[] = FRASES
   public instrucao: string = 'Traduza a frase:'
+  public progresso: number = 0
   public resposta: string = ''
-
   public rodada: number = 0
-  public rodadaFrase: Frase
+  public rodadaFrase!: Frase;
+  public tentativas: number = 3
 
   constructor() {
-    this.rodadaFrase = this.frases[this.rodada]
-    console.log(this.rodadaFrase)
+    this.atualizaRodada()
   }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void{
+  }
+
   public atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value
-    //console.log(this.resposta)
   }
 
   public verificarResposta(): void {
     if (this.rodadaFrase.frasePtBr === this.resposta) {
-      alert('A tradução está correta')
+
       this.rodada++
-      this.rodadaFrase = this.frases[this.rodada]
+      this.progresso += (100 / this.frases.length)
+
+      if (this.rodada === 4) {
+        this.encerrarJogo.emit('Vitoria')
+      }
+
+      this.atualizaRodada()
+
     } else {
-      alert('A tradução está errada')
+
+      this.tentativas--
+
+      if (this.tentativas === -1) {
+        this.encerrarJogo.emit('Derrota')
+      }
     }
+  }
+
+  public atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada]
+    this.resposta = ''
   }
 
 }
--- app1/src/app/tentativas/tentativas.component.ts
+++ app1/src/app/tentativas/tentativas.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Coracao } from '../shared/coracao.model';
 
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
 
-  public coracaoVazio: string ='/assets/coracao_vazio.png'
-  public coracaoCheio: string ='/assets/coracao_cheio.png'
+  @Input()
+  public tentativas!: number;
+
+  public  coracoes: Coracao[] =[
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true)
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  ngOnChanges(): void {
+    console.log(this.coracoes)
+    if(this.tentativas!== this.coracoes.length){
+      let indice = this.coracoes.length - this.tentativas
+      this.coracoes[indice -1].cheio = false
+    }
   }
 
 }
